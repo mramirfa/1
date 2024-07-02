627e190c6b77587d9e584c22fe195ff0e2fc983f
aMIr <166694766+mramirfa@users.noreply.github.com>
2024-07-02 02:47:13 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,99 @@
+// slid1
+const swiper1 = new Swiper(".swiper-container1", {
+  loop: false,
+  speed: 202,
+  effect: "fade",
+
+  autoplay: {
+    daly: 4000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+});
+
+// slid
+const swiper2 = new Swiper(".swiper-container2", {
+  loop: false,
+  speed: 250,
+  spaceBetween: 8,
+
+  breakpoints: {
+    250: {
+      slidesPerView: 2,
+    },
+    340: {
+      slidesPerView: 2.5,
+    },
+    410: {
+      slidesPerView: 3,
+    },
+    530: {
+      slidesPerView: 2,
+    },
+    700: {
+      slidesPerView: 2.4,
+    },
+    960: {
+      slidesPerView: 3.2,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next2",
+    prevEl: ".prev2",
+  },
+});
+
+const swiper3 = new Swiper(".swiper-container3", {
+  loop: false,
+  speed: 250,
+  spaceBetween: 8,
+
+  breakpoints: {
+    250: {
+      slidesPerView: 2,
+    },
+    340: {
+      slidesPerView: 2.5,
+    },
+    410: {
+      slidesPerView: 3,
+    },
+    530: {
+      slidesPerView: 2,
+    },
+    700: {
+      slidesPerView: 2.6,
+    },
+    960: {
+      slidesPerView: 3.2,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next3",
+    prevEl: ".prev3",
+  },
+});
